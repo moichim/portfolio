01ffe7e2c6827b59504022971a0402aaaa13cd16
Jan Jáchim <jachim@ntc.zcu.cz>
2023-09-14 16:16:27 +0200

    fixing transitions

--- components/ui/PageTransition.tsx
+++ components/ui/PageTransition.tsx
@@ -5,7 +5,7 @@ type PageTransitionProps = HTMLMotionProps<'div'>
 export type PageTransitionRef = React.ForwardedRef<HTMLDivElement>
 
 function PageTransition({ children, ...rest }: PageTransitionProps, ref: PageTransitionRef) {
-	const initial = { opacity: 0, y: "50vh", position: "absolute", top: "0px", width: "100%" }
+	const initial = { opacity: 0, y: "50vh", position: "absolute", top: 0, width: "100%" }
 	const animate = { opacity: 1, y: "0vh", position: "relative" }
 	const exit = { opacity: 0, y: "50vh", position: "absolute" }
 
@@ -14,9 +14,9 @@ function PageTransition({ children, ...rest }: PageTransitionProps, ref: PageTra
 	return (
 		<motion.div
 			ref={ref}
-			initial={initial}
-			animate={animate}
-			exit={exit}
+			initial={{ opacity: 0, y: "50vh", position: "absolute", top: 0, width: "100%" }}
+			animate={{ opacity: 1, y: "0vh", position: "relative" }}
+			exit={{ opacity: 0, y: "50vh", position: "absolute" }}
 			transition={transition}
 			{...rest}
 		>
--- components/ui/ProjectTransition.tsx
+++ components/ui/ProjectTransition.tsx
@@ -5,7 +5,7 @@ type PageTransitionProps = HTMLMotionProps<'div'>
 export type PageTransitionRef = React.ForwardedRef<HTMLDivElement>
 
 function ProjectTransition({ children, ...rest }: PageTransitionProps, ref: PageTransitionRef) {
-	const initial = { opacity: 0, scale: .7, position: "absolute", top: "0px" }
+	const initial = { opacity: 0, scale: .7, position: "absolute", top: 0 }
 	const animate = { opacity: 1, scale: 1, position: "relative" }
 	const exit = { opacity: 0, scale: .7, position: "absolute" }
 
@@ -14,9 +14,9 @@ function ProjectTransition({ children, ...rest }: PageTransitionProps, ref: Page
 	return (
 		<motion.div
 			ref={ref}
-			initial={initial}
-			animate={animate}
-			exit={exit}
+			initial={{ opacity: 0, scale: .7, position: "absolute", top: 0 }}
+			animate={{ opacity: 1, scale: 1, position: "relative" }}
+			exit={{ opacity: 0, scale: .7, position: "absolute" }}
 			transition={transition}
 			{...rest}
 		>
