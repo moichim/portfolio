3bdfeeb3db72105fa6c51c2a568663ba453ab8d5
Jan Jáchim <jachim@ntc.zcu.cz>
2023-09-18 15:22:53 +0200

    fix _app.tsx links to CSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,18 @@
 import GlobalLayout from '@/components/layout/global/GlobalLayout';
 import GlobalProviders from '@/context/GlobalProviders';
 import LayoutInnerProviders from '@/context/LayoutInnerProviders';
-import '@/styles/globals.scss';
-import type { AppProps } from 'next/app';
-import { Router, useRouter } from 'next/router';
 
-import "@/components/partials/content/Magnifier.scss";
+import type { AppProps } from 'next/app';
+
 import "@fontsource/ibm-plex-sans";
 import "@fontsource/ibm-plex-sans/500-italic.css";
 import "@fontsource/ibm-plex-sans/500.css";
 import "@fontsource/ibm-plex-sans/700.css";
 
-import PageTransition, { useAsPathWithoutHash } from '@madeinhaus/nextjs-page-transition';
+import '@/styles/globals.scss';
+import "@/components/content/Magnifier.scss";
+
+import { useAsPathWithoutHash } from '@madeinhaus/nextjs-page-transition';
 
 
 export default function App({ Component, pageProps, router }: AppProps) {
